fdb41c35d7821cb35b6a30ea426c8e8d0e1d3ec8
marko <masscry@gmail.com>
2024-05-11 20:38:26 +0300

    common: assert with message

--- lib/common/macro.hh
+++ lib/common/macro.hh
@@ -7,3 +7,9 @@
         corey::panic("{}:{}: assertion failed ({})", __FILE__, __LINE__, #EXPR); \
     } \
 } while (0)
+
+#define COREY_ASSERT_MSG(EXPR, FMT, ...) do { \
+    if (!(EXPR)) { \
+        corey::panic("{}:{}: assertion failed ({}) - " FMT, __FILE__, __LINE__, #EXPR, __VA_ARGS__); \
+    } \
+} while (0)
