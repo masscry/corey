143ce1d93b8d188957b24211e84eb1bfa80d2ec1
marko <masscry@gmail.com>
2024-05-11 19:58:26 +0300

    common: simplify assert and fix warnings

--- lib/common/macro.hh
+++ lib/common/macro.hh
@@ -2,4 +2,8 @@
 
 #define COREY_UNREACHABLE __builtin_unreachable
 
-#define COREY_ASSERT(EXPR) ((EXPR) ?: (corey::panic("{}:{}: assertion failed ({})", __FILE__, __LINE__, #EXPR), false))
+#define COREY_ASSERT(EXPR) do { \
+    if (!(EXPR)) { \
+        corey::panic("{}:{}: assertion failed ({})", __FILE__, __LINE__, #EXPR); \
+    } \
+} while (0)
